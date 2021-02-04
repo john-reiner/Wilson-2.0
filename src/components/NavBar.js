ec95a18820d2335f4a6e998548fc4cbfd54801ef
john-reiner <johnreiner1990@gmail.com>
2021-02-04 14:49:44 -0500

    connected to local storage for token

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -28,7 +28,7 @@ export default function NavBar(props) {
                     <Nav.Link>Completed</Nav.Link>
                 </LinkContainer>
             </Nav>
-                <Button variant="outline-secondary">{props.loggedinUser ? props.loggedinUser.username : 'Please Login'}</Button>
+                <Button variant="outline-secondary">{props.loggedinUser ? props.loggedinUser : 'Please Login'}</Button>
         </Navbar>
     )
 }

eb8eeaada4bc8854d2d90b65b3fe772ede99b99e
john-reiner <johnreiner1990@gmail.com>
2023-07-28 06:53:33 -0400

    converting .js -> .tsx - DeleteModalConfirmation

--- src/packages/Wilson/packages/global/DeleteModalConfirmation.tsx
+++ src/packages/Wilson/packages/global/DeleteModalConfirmation.tsx
@@ -2,9 +2,25 @@ import React from 'react'
 
 import { Modal, Button, Group, Text, Stack, Divider } from '@mantine/core'
 
-export default function DeleteModalConfirmation(props) {
+interface DeleteModalConfirmationProps {
+    route: string
+    successFunction: () => void
+    opened: boolean
+    setOpened: React.Dispatch<React.SetStateAction<boolean>>
+    item: string
+}
 
-    const deleteItem = (route) => {
+export default function DeleteModalConfirmation({
+    route,
+    successFunction,
+    opened,
+    setOpened,
+    item
+}: DeleteModalConfirmationProps) {
+
+    const deleteItem = (
+        route: string
+    ) => {
         fetch(route, {
             method: 'DELETE',
             headers: {
@@ -14,8 +30,8 @@ export default function DeleteModalConfirmation(props) {
             })
         .then(response => response.json())
         .then(payload => {
-            props.successFunction()
-            props.setOpened(false)
+            successFunction()
+            setOpened(false)
         })
         .catch(errors => {
             console.error(errors)
@@ -24,16 +40,16 @@ export default function DeleteModalConfirmation(props) {
 
     return (
         <Modal
-            opened={props.opened}
-            onClose={() => props.setOpened(false)}
-            title={`Delete ${props.item}?`}
+            opened={opened}
+            onClose={() => setOpened(false)}
+            title={`Delete ${item}?`}
         >
             <Stack>
-                <Text>{`Are you sure you want to delete this ${props.item}?`}</Text>
+                <Text>{`Are you sure you want to delete this ${item}?`}</Text>
                 <Divider my="xs" />
                 <Group position="apart" spacing="sm">
-                    <Button variant="outline" color="grey" onClick={() => props.setOpened(false)}>Back</Button>
-                    <Button variant="outline" color="red" onClick={() => deleteItem(props.route)}>Delete</Button>
+                    <Button variant="outline" color="grey" onClick={() => setOpened(false)}>Back</Button>
+                    <Button variant="outline" color="red" onClick={() => deleteItem(route)}>Delete</Button>
                 </Group>
             </Stack>
         </Modal>
